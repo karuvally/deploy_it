79617b8d96fe989ea9daf3afde1e809facf3156d
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-03-31 18:13:10 +0530

    added main()

---
+++ build.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# deploy_it, alpha release
+# Copyright 2019, Aswin Babu Karuvally
+
+# import the serious stuff
+import json
+import argparse
+import shutil
+
+
+# the main function
+def main():
+    pass
