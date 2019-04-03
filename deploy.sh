59e635d767a9685df3de0dd6e803f2925172ff31
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-04-03 20:09:38 +0530

    added main()

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # deploy_it, alpha release
 # Copyright 2019, Aswin Babu Karuvally
+
+# import serious stuff
+
+
+# the main function
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
