1ed2b8b02d455813e516b5dd5610a2396c595033
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-07-13 11:35:16 +0530

    added uninstall.py

---
+++ uninstall.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# deploy_it uninstall script
+# Copyright 2019, Aswin Babu Karuvally
+
+
+# the main function
+def main():
+    pass
+
+
+# call the main function
+if __name__ == "__main__":
+    main()
+
