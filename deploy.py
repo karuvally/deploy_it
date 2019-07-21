7a6e131b23fb9b9c314d35d7fbcc5c4a7840fcc9
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-07-21 12:13:32 +0530

    moved logging to setup_logging()

--- deploy.py
+++ deploy.py
@@ -13,6 +13,21 @@ from distutils.dir_util import copy_tree
 import pdb # debug
 
 
+def setup_logging():
+    format_string = "[%(asctime)s] %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+
+    logging.basicConfig(
+        filename = os.path.join("build.log"),
+        level = logging.DEBUG,
+        format = format_string,
+        datefmt = date_format
+    )
+
+    # print logs to stderr
+    logging.getLogger().addHandler(logging.StreamHandler())
+
+
 # execute command
 def execute_command(command):
     return_code = subprocess.call(command, shell=True)
@@ -69,23 +84,11 @@ def install_virtualenv():
 
 # the main function
 def main():
-    # setup logging
-    format_string = "[%(asctime)s] %(message)s"
-    date_format = "%Y-%m-%d %H:%M:%S"
-
-    logging.basicConfig(
-        filename = os.path.join("build.log"),
-        level = logging.DEBUG,
-        format = format_string,
-        datefmt = date_format
-    )
-
-    # print logs to stderr
-    logging.getLogger().addHandler(logging.StreamHandler())
+    setup_logging()
 
     # check if user is root
     if os.getuid() != 0:
-        logging.critical("script cannot be run as normal user, exiting")
+        logging.critical("please run the deployer as root")
         sys.exit(1)
 
     # read configuration
